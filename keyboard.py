508800c6fb5faaf990224f95417b31586df686eb
RomagnaMarco <37676804+RomagnaMarco@users.noreply.github.com>
2019-04-26 16:06:09 -0700

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,10 +2,10 @@ import keyboard
 import time
 
 def TestKeyboard(keyInput):
-    keyboard.press_and_release(keyInput)
+   keyboard.press_and_release(keyInput)
 
 test = "Hi my name is Vinh"
 
 for char in test:
     TestKeyboard(char)
-
+    
